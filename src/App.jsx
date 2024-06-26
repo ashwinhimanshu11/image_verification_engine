bb9c98ca71f7e303764b70091cab7c7ef19f5dc4
SaumyaGupta26 <guptasaumya026@gmail.com>
2024-06-26 14:23:34 +0530

    Add my React project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Nav from './components/Nav'
+import Footer from'./components/Footer'
+import ResultPage from './components/ResultPage'
+import InputComponent from './components/InputComponent'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Nav/> */}
+      <InputComponent/> <br />
+      <ResultPage/> 
+      {/* <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className="footer-1">
+          <p>India</p>
+        </div>
+
+        <div className="footer-2">
+          <div className="f-2">
+            <p>About</p>
+            <p>Advertising</p>
+            <p>Business</p>
+            <p>How Search Works</p>
+          </div>
+
+          <div className="f-2">
+            <p>Privacy</p>
+            <p>Terms</p>
+            <p>Settings</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
